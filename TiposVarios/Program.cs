b177f4642e3dabd66e982bd77f85c80eb8894dd0
Pedro Santos <pedrosantosobral@gmail.com>
2018-02-22 16:43:47 +0000

    first commit

---
+++ TiposVarios/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TiposVarios
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string n = "Isto é uma string\u00A9";
+            string v = @"testedecodigo\u03A9";
+            string c = "a" + 2;
+            string d = "ABC" + n;
+            Console.WriteLine(n);
+            Console.WriteLine(v);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+        }
+    }
+}
+ 
